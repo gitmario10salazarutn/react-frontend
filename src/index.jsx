e3461cc74e5485967bf05af2452fdf319f38aa72
Mario Salazar <salazar10mario.1995@gmail.com>
2023-05-15 16:18:59 -0500

    First Commit

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import "./css/flags.css";
+import "./css/index.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css"; // theme
+import "primereact/resources/primereact.css"; // core css
+import "primeicons/primeicons.css";
+
+const F = () => {
+    return (
+        <React.StrictMode>
+            <BrowserRouter>
+                <Login />
+            </BrowserRouter>
+        </React.StrictMode>
+    );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(F());
