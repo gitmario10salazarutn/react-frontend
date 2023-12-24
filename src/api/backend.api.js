14ae765d0eed74c93c31d1a5bf0509dfae8c7b96
Mario Salazar <mariosalazar.ms.10@gmail.com>
2023-12-24 04:20:28 -0500

    Updated the Backend URL

--- src/api/backend.api.js
+++ src/api/backend.api.js
@@ -1,26 +1,26 @@
 import axios from "axios";
 
-const url = "https://mario10salazar-apputn.onrender.com"
+const url = "https://app-9f0a1fa0-f478-4848-a93a-b9847d089c72.cleverapps.io/"
 
-const getUsers = async()=> await (await axios.get(url+"/users/get_users"));
+const getUsers = async () => await (await axios.get(url + "/users/get_users"));
 
-const getGenders = async()=> await (await axios.get(url+"/users/get_genders"));
+const getGenders = async () => await (await axios.get(url + "/users/get_genders"));
 
-const getRols = async()=> await (await axios.get(url+"/users/get_rols"));
+const getRols = async () => await (await axios.get(url + "/users/get_rols"));
 
-const getLanguageLevels = async()=> await (await axios.get(url+"/users/get_knowledgelevels"));
+const getLanguageLevels = async () => await (await axios.get(url + "/users/get_knowledgelevels"));
 
-const get_LanguagesProgramming = async()=> await (await axios.get(url+"/users/get_LanguagesProgramming"));
+const get_LanguagesProgramming = async () => await (await axios.get(url + "/users/get_LanguagesProgramming"));
 
-const login = async(data)=> await axios.post(url+"/users/login_user", data)
+const login = async (data) => await axios.post(url + "/users/login_user", data)
 
-const getUser = async(id_user) => await axios.get(url+"/users/get_userbyusername/"+id_user)
+const getUser = async (id_user) => await axios.get(url + "/users/get_userbyusername/" + id_user)
 
-const getLanguage = async(id_user) => await axios.get(url+"/users/get_languagebyuser/"+id_user)
+const getLanguage = async (id_user) => await axios.get(url + "/users/get_languagebyuser/" + id_user)
 
-const getEducationByUser = async(id_user) => await axios.get(url+"/users/get_educationbyuser/"+id_user)
+const getEducationByUser = async (id_user) => await axios.get(url + "/users/get_educationbyuser/" + id_user)
 
-const getEducationById = async(id) => await axios.get(url+"/users/get_educationbyid/"+id)
+const getEducationById = async (id) => await axios.get(url + "/users/get_educationbyid/" + id)
 
 
 /**
@@ -28,16 +28,16 @@ const getEducationById = async(id) => await axios.get(url+"/users/get_educationb
  * @param {Number} id Enter the language Id to search
  * @returns This method retur a json object
  */
-const getLanguageById = async(id) => await axios.get(url+"/users/get_languagebyid/"+id)
+const getLanguageById = async (id) => await axios.get(url + "/users/get_languagebyid/" + id)
 
 /**
  * 
  * @param {Number} id Enter the language ID to delete
  * @returns This method return a error message or success message
  */
-const deleteLanguage = async(id) => await axios.get(url+"/users/get_languagebyuser/"+id)
+const deleteLanguage = async (id) => await axios.get(url + "/users/get_languagebyuser/" + id)
 
-const deleteUser = async(id_user) => await axios.delete(`${url}/users/delete_user/${id_user}`)
+const deleteUser = async (id_user) => await axios.delete(`${url}/users/delete_user/${id_user}`)
 
 export {
     getUsers,
